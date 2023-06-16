42a991346273e2b2a4655cd35e2b04c2dec5ae61
Ricardo Diaz <rd.diazvega@ugto.mx>
2023-06-16 13:45:19 -0600

    Se agregaron input al formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Modal, Text, SafeAreaView, StyleSheet } from 'react-native'
+import { Modal, Text, SafeAreaView, ScrollView, StyleSheet, TextInput, View } from 'react-native'
 
 const Formulario = (props) => {
   return (
@@ -8,10 +8,44 @@ const Formulario = (props) => {
         visible={props.modalVisible}
     >
       <SafeAreaView style = {styles.contenido}>
-        <Text style={styles.titulo}> Nueva {''}
-            <Text style = {styles.tituloBold}>Cita</Text>
-        </Text>
+        
+        <ScrollView>
 
+          <Text style={styles.titulo}> Nueva {''}
+              <Text style = {styles.tituloBold}>Cita</Text>
+          </Text>
+
+          <View style={styles.campo}>
+            <Text style={styles.label}> Nombre de Cliente</Text>
+            <TextInput 
+                style={styles.input}
+                placeholder='Nombre de Cliente'
+                placeholderTextColor={'#666'}
+            />
+          </View>
+
+          <View style={styles.campo}>
+            <Text style={styles.label}> Correo Electrónico</Text>
+            <TextInput 
+                style={styles.input}
+                placeholder='Correo Electrónico'
+                placeholderTextColor={'#666'}
+                keyboardType='email-address'
+            />
+          </View>
+
+          <View style={styles.campo}>
+            <Text style={styles.label}> Número de teléfono</Text>
+            <TextInput 
+                style={styles.input}
+                placeholder='Número de teléfono'
+                placeholderTextColor={'#666'}
+                keyboardType='number-pad'
+            />
+          </View>
+
+        </ScrollView>
+        
       </SafeAreaView>
     </Modal>
   )
@@ -31,6 +65,23 @@ const styles = StyleSheet.create({
   },
   tituloBold: {
     fontWeight: '900'
+  },
+  campo:{
+    marginTop: 10,
+    marginHorizontal: 30,
+  },
+  label: {
+    color: '#FFF',
+    marginBottom: 10,
+    marginTop: 15,
+    fontSize: 20,
+    fontWeight: '600'
+
+  },
+  input:{
+    backgroundColor: '#FFF',
+    padding: 15,
+    borderRadius: 10,
   }
 
 })

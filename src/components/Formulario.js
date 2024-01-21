c863f82ab2807b96eafdaf99393ed52efafd6843
Ricardo Diaz <rd.diazvega@ugto.mx>
2024-01-20 23:20:58 -0600

    creación de estilos a nuevo componente

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,5 +1,11 @@
 import React from 'react'
-import { Modal, Text } from 'react-native'
+import { 
+  Modal, 
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Pressable, 
+    } from 'react-native'
 
 const Formulario = (props) => {
   return (
@@ -7,9 +13,33 @@ const Formulario = (props) => {
         animationType='slide' 
         visible={props.modalVisible}
     >
-        <Text> Desde Modal</Text>
+      <SafeAreaView style={styles.container}>
+        <Text style = {styles.titulo}>
+          Nueva {' '}
+          <Text style = {styles.tituloBold}>
+             Cita
+          </Text>
+        </Text>
+      </SafeAreaView>
     </Modal>
   )
 }
+
+const styles = StyleSheet.create({
+  container:{
+    backgroundColor: '#6D28D9',
+    flex: 1
+  },
+  titulo:{
+    color: '#FFF',
+    fontSize: 30,
+    fontWeight: '600',
+    textAlign: 'center',
+    marginTop: 30
+  },
+  tituloBold: {
+    fontWeight: '900'
+  }
+})
 
 export default Formulario
